dc22457cbf1454f25ab4874f8aa8b1642c358d8a
Alfredo Gonzalez <56003346+Dakoth@users.noreply.github.com>
2020-05-10 04:10:25 -0700

    updated main with stuff from the light game

--- source/main.c
+++ source/main.c
@@ -71,149 +71,158 @@ void TimerSet(unsigned long M) {
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 //State Machine stuff 
 
-
-enum States {Start, Wait, incHold, decHold, reset} state; 
-
-unsigned char tmpA; //global variables 
-
-unsigned char tmpC;//= 0x00;
-unsigned char tmpD;
-unsigned char i = 0;	//Used to count up to 1 second, for the inc hold states 
+enum States {Start, A, B, C, D, buttPress, Wait} state; 
+unsigned char tmpA; 
+unsigned char tmpB;
+unsigned char tmpC; 
+unsigned char ableToPause = 1; //Bool value. Used to determine if able to be paused  
 
 void Tick() {
-	//unsigned char tmpA = PINA;
-	switch(state) { //transitions
-		case Start:
-			tmpC = 0; 
-			state = Wait;
-			break;
-		
-		case Wait:
-			if ( ((tmpA & 0x01) == 0x01) && ((tmpA & 0x02) == 0x00) ) { //if PA0 && !PA1
-				state = incHold;
-
-				if (tmpC < 9) { 
-					++tmpC; 
-					i = 0;
-					LCD_ClearScreen();
-					LCD_WriteData(tmpC + '0');
-
-				} //TRANSITION ACTION
-
-			} 
-			else if ( ((tmpA & 0x02) == 0x02) && ((tmpA & 0x01) == 0x00) ) //if PA1 && !PA0
-			{
-				state = decHold;
-
-				if (tmpC > 0) { 
-					--tmpC; 
-					i = 0; 
-					LCD_ClearScreen();
-					LCD_WriteData(tmpC + '0');
-				}	//TRANSITION ACTION
-
-			}
-			else if ((tmpA & 0x03) == 0x03) { //if PA1 && PA0
-				state = reset;
-				tmpC = 0; 	//SHOULD BE 0 , SETTING TO 0x7F  FOR TEST
-			}
-			else { //If none of the above
-				state = Wait;
-			}
+	switch(state) { //transitions:
+		case Start: 
+			state = A;
 			break;
 
-		case incHold: //Might need a transition for resetting?
-			if( ((tmpA & 0x01) == 0x01) && ((tmpA & 0x02) == 0x00) ) { //if PA0 && !PA1 
-				state = incHold;
-
-				//NEW 
-				if (tmpC < 9 && i == 10) { //if 1 second has passed 
-					++tmpC;
-					i = 0; //reset i
-					LCD_ClearScreen();
-					LCD_WriteData(tmpC + '0');
-				}
-
+		case A: 
+			if ( ((tmpA & 0x01) == 0x01) && (ableToPause == 1)  ) { //button pressed 1st time
+				state = buttPress;
+				ableToPause = 0;	
 			}
-	 			
-			else if ( (tmpA & 0x03) == 0x03  ) { //added this so can reset
-				state = reset;
-			}	
-			else { 
-				state = Wait;
-			}			
-			break;
 
-		case decHold:
-			if( ((tmpA & 0x02) == 0x02) && ((tmpA & 0x01) == 0x00) ) { //if !PA0 && PA1 
+			//After releasing the button after the second time, able to be paused  normally as above  
+			else if ( ((tmpA & 0x01) == 0x00) && (ableToPause == 0)) {
+				//state = buttPress;
+				state = B;
+				ableToPause = 1;
+				
+			}
 			
-				state = decHold;
-
-				//NEW
-				if (tmpC > 0 && i == 10) { //If 1 sec has passed 
-					--tmpC;
-					i = 0; //reset i ; 
-					LCD_ClearScreen();
-					LCD_WriteData(tmpC + '0');
-				}
-
-			}
-			else if ( (tmpA & 0x03) == 0x03) {
-				state = reset;
-			}
-			else {
-				state = Wait;
+			else { //else go to next part of sequence
+				state = B;
+				//firstPress = 1; //ADDED 
 			}
 			break;
 
-		case reset: 
-			if ((tmpA & 0x03) == 0x03) {
-				state = reset;
+		case B:   
+			if ( ((tmpA & 0x01) == 0x01) && (ableToPause == 1)  ) { //button pressed 1st time
+				state = buttPress;
+				ableToPause = 0;	
 			}
-			else {
+
+			//After releasing the button after the second time, able to be paused  normally as above  
+			else if ( ((tmpA & 0x01) == 0x00) && (ableToPause == 0)) {
+				//state = buttPress;
+				state = C;
+				ableToPause = 1;	
+			}
+			
+			else { //else go to next part of sequence
+				state = C;
+			}
+			break;
+
+		case C:      
+			if ( ((tmpA & 0x01) == 0x01) && (ableToPause == 1)  ) { //button pressed 1st time
+				state = buttPress;
+				ableToPause = 0;	
+			}
+
+			//After releasing the button after the second time, able to be paused  normally as above  
+			else if ( ((tmpA & 0x01) == 0x00) && (ableToPause == 0)) {
+				//state = buttPress;
+				state = D;
+				ableToPause = 1;	
+			}
+			
+			else { //else go to next part of sequence
+				state = D;
+			}
+			break;
+
+		case D:     
+			if ( ((tmpA & 0x01) == 0x01) && (ableToPause == 1)  ) { //button pressed 1st time
+				state = buttPress;
+				ableToPause = 0;	
+			}
+
+			//After releasing the button after the second time, able to be paused  normally as above  
+			else if ( ((tmpA & 0x01) == 0x00) && (ableToPause == 0)) {
+				//state = buttPress;
+				state = D;
+				ableToPause = 1;	
+			}
+			
+			else { //else go to next part of sequence
+				state = A;
+			}
+			break;
+
+		case buttPress:
+			if ( (tmpA & 0x01) == 0x01) {
+				state = buttPress;
+			}
+			else { 
+				state = Wait;	
+			}
+			break;
+
+		case Wait:  
+			if ( (tmpA & 0x01) == 0x01 ) { //If button is pressed 
+				state = A;
+				//tmpC = 0x01;	
+			}
+			else { //else if button not pressed, just stay 
 				state = Wait;
 			}
+			break;
+			
+		default: 
+			state = A;
+			break;
+	}
+
+	switch (state) { //actions
+		case A:  	// ... 001
+			tmpB = (tmpB & 0x00) | 0x01; 
+			break;
+
+		case B: 	//010
+			tmpB = (tmpB & 0x00) | 0x02;
+			break;
+
+		case C: 	//100 
+			tmpB = (tmpB & 0x00) | 0x04;
+			break;
+
+		case D: 	//010
+			tmpB = (tmpB & 0x00) | 0x02;
+			break;
+
+		case buttPress: 
+			//tmpB = (tmpB & 0x0F) | 0x08; //for debugging 
+						    //Sets PB3 to 1 
+			break;
+
+		case Wait: 
 			break;
 
 		default: 
-			//shouldn't go here
-			state = Start;
-			break;					
-	}
-
-	switch(state) { 
-		case Wait:
 			break;
 
-		case incHold:
-			//if (tmpC < 9) { ++tmpC; }
-			i++; 
-			break;
-		
-		case decHold:
-			//if (tmpC > 0) { --tmpC; }
-			i++;
-			break;
-		
-		case reset: 
-			tmpC = 0;
-			LCD_ClearScreen();
-			LCD_WriteData(tmpC + '0');
-			break;
-		default:
-			break;
 	}
 }
+
 
 int main(void) {
     /* Insert DDR and PORT initializations */
 	DDRA = 0x00; PORTA = 0xFF;
 
+	DDRB = 0xFF; PORTB = 0x00;
 	DDRC = 0xFF; PORTC = 0x00; 	//LCD data lines
 	DDRD = 0xFF; PORTD = 0x00;	//LCD control lines 
 
 	//Timer stuff
-	TimerSet(100);
+	TimerSet(300);
 	TimerOn();
 
 	//initalizes LCD display
@@ -222,6 +231,7 @@ int main(void) {
 	//Starting at position 1, writes message
 	//LCD_DisplayString(1, "Beep beep I'm a sheep");
 	tmpA = 0x00;
+	tmpB = 0x00;
 	tmpC = 0x00;
 	tmpD = 0x00;
 
@@ -231,6 +241,7 @@ int main(void) {
 	tmpA = ~PINA;
 
 	Tick();
+	PORTB = tmpB;
 	PORTC = tmpC;
 
 	while (!TimerFlag) {}
