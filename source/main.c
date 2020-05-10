736b130f47d5bb064c1b90cdd00a7e4ae71297de
Alfredo Gonzalez <alfredogonzalez1999@gmail.com>
2020-05-10 00:53:44 -0700

    Adding inital shit to github

---
+++ source/main.c
@@ -0,0 +1,37 @@
+/*	Author: agonz250
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab # 7 Exercise #1
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+//timer stuff 
+
+
+
+
+
+
+
+
+
+// States n stuff 
+
+
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+
+    /* Insert your solution below */
+    while (1) {
+
+    }
+    return 1;
+}
